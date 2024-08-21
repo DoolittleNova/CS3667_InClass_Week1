1cbfc6749c3835b0df06fbd524e6ddb1c7c6e67c
DoolittleNova <doolittletr@appstate.edu>
2024-08-21 09:27:08 -0400

    Initial commit

---
+++ Demo.java
@@ -0,0 +1,14 @@
+/*
+ * Author:      Nova Doolittle
+ * Date:        08/21/2024 (Fall 2024)
+ * Filename:    Demo.java
+ * Assignment:  In Class: Java & Git Practice (08/21)
+ */
+
+// Container class for assignment
+public class Demo {
+    // Main method
+    public static void main() {
+        
+    }
+}
