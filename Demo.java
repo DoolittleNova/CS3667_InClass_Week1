8a1ca2c383ede383a9c25119cbdd111768872d85
DoolittleNova <doolittletr@appstate.edu>
2024-08-23 09:25:09 -0400

    Commented up, complete

--- Demo.java
+++ Demo.java
@@ -14,18 +14,27 @@ import java.util.Scanner;
 public class Demo {
     // Main method
     public static void main(String[] args) {
+        /*
+         * Begin user Input block
+        */
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter dog type (poodle or lab): ");
         String in = scan.nextLine(); // Get dog type from user.
+
+        // We ought to sanitize input, but I can't remember the lowercase function or implementation. Nor do I particularly want to bring the regex library in.
         
-        // Some data validation
+        // Input validation
         while(!in.equals("lab") && !in.equals("poodle")) {
             System.out.print("Error: Input not a supported type of dog!\nEnter dog type: ");
             in = scan.nextLine();
         }
 
+        /*
+         * End User Input block
+         */
+
         // Assign dog accordingly
-        Dog doggy = in.equals("lab") ? new Lab() : new Poodle();
+        Dog doggy = in.equals("lab") ? new Lab() : new Poodle(); // My apologies for this implementation.
 
         // Get name of dog
         doggy.getName(scan);
